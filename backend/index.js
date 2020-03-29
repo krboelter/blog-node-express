b37bede94292bd167c3b9f216ebbf6a9417ae433
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-03-29 01:23:05 -0600

    changed from graphql to rest

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
-const server = require("./server");
 
 app.use(express.json());
-app.use("/api", server);
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  `*** Server listening on port: ${port} ***`;
+  `***Server listening on port ${port}])`;
 });
