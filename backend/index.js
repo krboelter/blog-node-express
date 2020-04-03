afcd1210f009a2eb056f74cb6cb357de165e6e00
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-04-02 22:03:06 -0600

    started okta implamentation

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const session = require("express-session");
+const { ExpressOIDC } = require("@okta/oidc-middleware");
 const app = express();
 
 app.use(express.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+  })
+);
+
+const oidc = new ExpressOIDC({
+  issuer: process.env.ISSUER,
+  client_id: process.env.CLIENT_ID,
+  client_secret: process.env.CLIENT_SECRET,
+  loginRedirectUri: "http://localhost:3000/authorization-code/callback",
+  scope: "openid profile",
+});
+
+app.use(oidc.router);
 
 app.get("/", (req, res, next) => {
   res.status(200).json({
